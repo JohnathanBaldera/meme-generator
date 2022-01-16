9538437e0043c33e1c11d4cdad7f877cbb368b9b
JohnathanBaldera <74841308+JohnathanBaldera@users.noreply.github.com>
2022-01-15 21:21:23 -0500

    Memes Generated

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,24 @@
+import { useState, useEffect } from 'react'
 import './Meme.css'
 
 export default function Meme() {
+
+    const [memes, setMemes] = useState([])
+    const [img, setImg] = useState({url: "images/TrollFace.png", description: "Troll Face"})
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => {
+            setMemes(data)
+        })
+    }, [])
+
+    function loadMeme() {
+        let random = Math.floor(Math.random() * 100)
+        setImg(memes.data.memes[random])
+    }
+
     return (
         <main>
             <div className='form'>
@@ -8,7 +26,8 @@ export default function Meme() {
                     <input type="text" className='input-field' placeholder='Top Text' />
                     <input type="text" className='input-field' placeholder='Bottom Text' />
                 </div>
-                <button type='submit' className='btn'>Get a new Meme Image 🖼️</button>
+                <button onClick={loadMeme} type='submit' className='btn'>Get a new Meme Image 🖼️</button>
+                <img src={img.url} alt={img.description} className='memeImg'/>
             </div>
         </main>
     )
